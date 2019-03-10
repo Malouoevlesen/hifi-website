01320e6b2278e0dc66736b4e13e033c9153a8b7f
Malou Øvlesen <malouoevlesen@gmail.com>
2019-03-10 11:13:51 +0100

    Added basic dynamicness for shop

---
+++ dist/js/shop.js
@@ -0,0 +1,32 @@
+function getProducts() {
+	  fetch('json/produkter.json')
+		.then(function(response) {
+		return response.json();
+	  })
+		.then(function(products) {
+			console.log(products);
+			renderProduct(products);
+			renderFunctionBar(products);
+		
+	  });
+}
+
+function renderFunctionBar(products) {
+	var elem = document.querySelector("#item-count");
+
+	elem.innerHTML = products.length + " ITEM(S)";
+}
+
+function renderProduct(products) {
+	var productsElem = document.querySelector(".products");
+	var html = "";
+	for (var i=0; i < products.length; i++) {
+		var product = products[i];
+		html += '<div class="product"><img src="' + product.image + '"><span class="product-title"><a href="">' + product.navn + ' </a></span><div class="clearfix"></div><span class="product-price">' + product.pris + '</span><div class="clearfix"></div><button>ADD TO CART</button></div>';
+	}
+	productsElem.innerHTML = html;
+}
+
+
+getProducts();
+
