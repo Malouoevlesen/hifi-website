ed24d461197a80c10cdd363438af55554d82979d
Malou Øvlesen <malouoevlesen@gmail.com>
2019-03-12 11:36:47 +0100

    Updated shop.js

--- dist/js/shop.js
+++ dist/js/shop.js
@@ -1,34 +1,54 @@
 /* JSON Products */
 
 function getProducts() {
-	  fetch('json/produkter.json')
-		.then(function(response) {
-		return response.json();
-	  })
-		.then(function(products) {
-			console.log(products);
-			renderProduct(products);
-			renderFunctionBar(products);
-		
-	  });
+	fetch('json/produkter.json')
+	  .then(function(response) {
+	  return response.json();
+	})
+	  .then(function(products) {
+		  console.log(products);
+		  renderProduct(products);
+		  renderFunctionBar(products);
+
+	});
 }
+getProducts();
+
+function getBrands() {
+	fetch('json/brand.json')
+	  .then(function(response) {
+	  return response.json();
+	})
+	  .then(function(brands) {
+		  console.log(brands);
+		  renderBrand(brands);
+	});
+}
+getBrands();
+
 
 function renderFunctionBar(products) {
-	var elem = document.querySelector("#item-count");
+  var elem = document.querySelector("#item-count");
 
-	elem.innerHTML = products.length + " ITEM(S)";
+  elem.innerHTML = products.length + " ITEM(S)";
 }
 
 function renderProduct(products) {
-	var productsElem = document.querySelector(".products");
-	var html = "";
-	for (var i=0; i < products.length; i++) {
-		var product = products[i];
-		html += '<div class="product"><img src="' + product.image + '"><span class="product-title"><a href="">' + product.navn + ' </a></span><div class="clearfix"></div><span class="product-price">' + product.pris + '</span><div class="clearfix"></div><button>ADD TO CART</button></div>';
-	}
-	productsElem.innerHTML = html;
+  var productsElem = document.querySelector(".products");
+  var html = "";
+  for (var i=0; i < products.length; i++) {
+	  var product = products[i];
+	  html += '<div class="product"><img src="' + product.image + '"><span class="product-title"><a href="">' + product.navn + ' </a></span><div class="clearfix"></div><span class="product-price">' + product.pris + '</span><div class="clearfix"></div><button>ADD TO CART</button></div>';
+  }
+  productsElem.innerHTML = html;
 }
 
-
-getProducts();
-
+function renderBrand(brands) {
+  var brandElem = document.querySelector("#brands-list");
+  var html = "";
+  for (var i=0; i < brands.length; i++) {
+	  var brand = brands[i];
+	  html += '<li><a href="">' + brand.brand + '</a></li>';
+  }
+  brandElem.innerHTML = html;
+}
